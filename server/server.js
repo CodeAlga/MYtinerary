6aa9a8cc0fc66cdd329a617a60ddd4a03c4a67db
CodeAlga <58303506+CodeAlga@users.noreply.github.com>
2020-03-03 14:53:39 +0100

    🔝🔝🔝 Added script to depley in Heroku 🔝🔝🔝

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 //const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const db = require("./keys").mongoURI;
 const mongoose = require("mongoose");
@@ -43,6 +44,18 @@ app.use("/activities", require("./routes/activities"));
 app.use("/users", require("./routes/users"));
 app.use("/authentication", require("./routes/authentication"));
 app.use("/comments", require("./routes/comments"));
+
+// //
+// // SERVE STATIC ASSETS IF PRODUCCTION
+// //
+if (process.env.NODE_ENV === "production") {
+  // // Set static folder
+  app.user(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Fanchy smanchy server is runing on port " + port);
